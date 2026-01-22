519028914c3c9a12cd586f66b697eebac1143cd0
m3hmet072 <115586805+m3hmet072@users.noreply.github.com>
2026-01-22 00:27:53 +0000

    Remove spaces from file names to fix URL encoding issues

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/AutoServiceHoute/' : '/',
+  publicDir: 'public',
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
