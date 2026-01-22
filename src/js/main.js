8b54d0e3758da03dd0ace3aae5f392d9a39bc52f
m3hmet072 <115586805+m3hmet072@users.noreply.github.com>
2026-01-22 00:24:23 +0000

    Fix MIME type errors by removing duplicate CSS links from HTML

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import '../css/base.css'
 import '../css/layout.css'
 import '../css/components.css'
 import '../css/pages/home.css'
+import '../css/footer.css'
 
 import { loadNavbar } from './navbar'
 
