7901becbc0d36b3310f266caede7eacdd173c8c4
m3hmet072 <115586805+m3hmet072@users.noreply.github.com>
2026-01-22 21:45:51 +0000

    Update site after Pages fix

---
+++ src/js/apk.js
@@ -0,0 +1,9 @@
+import '../css/base.css'
+import '../css/layout.css'
+import '../css/components.css'
+import '../css/pages/apk.css'
+import '../css/footer.css'
+
+import { loadNavbar } from './navbar'
+
+loadNavbar()
