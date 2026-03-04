82329a7fa69e94dc57a7e23407f819aaee7976bb
m3hmet072 <115586805+m3hmet072@users.noreply.github.com>
2026-03-04 16:57:39 +0000

    Rebuild docs assets and pages

--- docs/assets/main-CCt7hrFu.js
+++ docs/assets/main-CCt7hrFu.js
@@ -1,4 +1,4 @@
-import"./base-DEg1Y3Il.js";/* empty css               */import"./api-CLM0aaL7.js";import{l as t,r as o,a as i,b as s,c as r,i as n}from"./footer-section-Dj78KfEy.js";import{r as d}from"./pricing-section-BfjaHvDW.js";import{r as a}from"./stappen-section-_hBGvEJ8.js";function l(){const e=document.getElementById("score-section");e&&(e.innerHTML=`
+import"./base-DEg1Y3Il.js";/* empty css               */import"./api-CLM0aaL7.js";import{l as t,r as o,a as i,b as s,c as r,i as n}from"./footer-section-g6POTUEj.js";import{r as d}from"./pricing-section-RCoOjngv.js";import{r as a}from"./stappen-section-BVL8hlJi.js";function l(){const e=document.getElementById("score-section");e&&(e.innerHTML=`
 <section class="stats-section">
   <div class="stat-card">
     <div class="stat-icon">
